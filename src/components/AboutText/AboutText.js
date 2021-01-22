e1ccdb0b164535abf17ca09e7047e374904a2207
Jermdude7 <jeremys.g.greenfield@gmail.com>
2021-01-22 15:23:00 -0700

    updated portfolio

--- src/components/AboutText/AboutText.js
+++ src/components/AboutText/AboutText.js
@@ -5,7 +5,7 @@ export default function AboutText() {
     return (
         <>
         <p>
-            <img src={require("../../assets/headshot.png")} class="img-thumbnail mr-3" alt="profile-pic"/>
+            <img src={require("../../assets/Headshot.png")} class="img-thumbnail mr-3" alt="profile-pic"/>
     Hello my name is Jeremy Greenfield, I am a web developer. I have always liked computers and  technology. I am also very much a people person, so coding in a team is a blend made for me!
         </p>
         <br />
